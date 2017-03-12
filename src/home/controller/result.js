88beaeb2ee1f514ab8138f9ec0298e77affaa8c2
wuuw <w876629399u@qq.com>
2017-03-12 22:17:04 +0800

    修改首页form

--- src/home/controller/result.js
+++ src/home/controller/result.js
@@ -3,7 +3,12 @@
 import Base from './base.js';
 
 export default class extends Base {
+  init(http){
+    super.init(http);
+  }
   indexAction() {
+    let searchType = this.http.query.type;
+    this.assign('searchType', searchType);
     return this.display();
   }
 }
