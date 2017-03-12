7427a13111ee23eab945ab99ee710bfb0da522d5
wuuw <w876629399u@qq.com>
2017-03-12 19:34:14 +0800

    将搜索功能独立为一个控制器

--- src/home/controller/base.js
+++ src/home/controller/base.js
@@ -4,4 +4,8 @@ export default class extends think.controller.base {
   /**
    * some base method in here
    */
+   indexAction(){
+     //auto render template file index_index.html
+     return this.display();
+   }
 }
--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,23 +1,15 @@
 'use strict';
 import Base from './base.js';
-import url from 'url';
 
 export default class extends Base {
   /**
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
   aboutAction(){
     return this.display();
   }
   teamAction(){
-    return this.display();
-  }
-  resultAction(){
     return this.display();
   }
 }
