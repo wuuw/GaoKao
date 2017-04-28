50eaaafc69fa7763823429a46eae561f280b116f
FiveSheep <fivesheep@Fin-XdeiMac.local>
2017-04-28 23:12:08 +0800

    test db.js

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -7,8 +7,8 @@ export default {
   type: 'mysql',
   adapter: {
     mysql: {
-      host: '127.0.0.1',
-      port: '',
+      host: '192.168.1.222',
+      port: '3306',
       database: 'referencesystem_new',
       user: 'root',
       password: '123456',
