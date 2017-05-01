35c4ff699532ffbce1b3d3d79b57a16c31b0f9fb
FiveSheep <fivesheep@Fin-XdeiMac.local>
2017-05-01 21:34:30 +0800

    修改数据库名称

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -11,8 +11,12 @@ export default {
     mysql: {
       // host: '192.168.2.222',
       host: '127.0.0.1',
+
       port: '3306',
-      database: 'referencesystem_new',
+      
+      // database: 'referencesystem_new',
+      database: 'referencesystem',
+
       user: 'root',
       password: '123456',
       prefix: 'ref_',
