7bd19bb8bec075dd65781562ece09b01cbe7140a
wuuw <w876629399u@qq.com>
2017-05-02 21:28:09 +0800

    db

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -9,7 +9,7 @@ export default {
   type: 'mysql',
   adapter: {
     mysql: {
-      host: '192.168.1.222',
+      host: '127.0.0.1',
       port: '3306',
       database: 'referencesystem_new',
       user: 'root',
