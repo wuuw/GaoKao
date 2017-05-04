b0f5f1820dd2fe46f2164c08f721a7363628a2e1
wuuw <w876629399u@qq.com>
2017-05-04 20:35:22 +0800

    向数据库增加学校ID表格

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,38 +1,28 @@
 'use strict';
 import Base from './base.js';
-
+import fs from 'fs';
 export default class extends Base {
   /**
    * index action
    * @return {Promise} []
    */
 
-  async testAction() {
-    let collegeModel = this.model('college');
-
-    // let record = await collegeModel.where("Cyear = 2014 and Cbatch = '本科第一批'").field('Ccutoffline as min, Cequipotential as eq').select();
-    // let record = await collegeModel.where("Cyear = 2014 and Cbatch = '本科第二批'").field('Ccutoffline as min, Cequipotential as eq').select();
-    let record = await collegeModel.where("Cyear = 2014").field('Ccutoffline as min, Cequipotential as eq').select();
-
-    let data = [];
-
-    record.forEach(item => {
-      data.push({min: item.min, eq: item.eq});
-    });
-
-    let json = {
-      data: data
-    };
-
-    this.assign(json);
-
-    return this.display();
-  }
-
   aboutAction() {
     return this.display();
   }
   teamAction() {
+    return this.display();
+  }
+  saveAction() {
+    let All = JSON.parse(fs.readFileSync(__dirname + '/schoolID.json').toString());
+
+    for (let i = 1; i < 31; i++) {
+      for (let j in All.data[i].college) {
+        // console.log(j, All.data[i].college[j]);
+        this.model('college').query(`insert into ref_collegeID(id, Cname) value (${j}, '${All.data[i].college[j]}')`);
+      }
+    }
+
     return this.display();
   }
 }
